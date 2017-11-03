5392fc72c21b0867d102b9fef4e75fd62c5b3646
Michael Rooney <m.rooney93@gmail.com>
2017-11-03 15:32:04 +0000

    Almost done

--- Assignment 2/matmul_transpose.c
+++ Assignment 2/matmul_transpose.c
@@ -90,13 +90,17 @@ long long time_matmul(int size){
 	return time_taken;
 }
 
-#define NUM_ITERATIONS 1
-#define NUM_SIZES 1
-const int sizes[NUM_SIZES] = { 2000 };
+#define NUM_ITERATIONS 10
+#define NUM_SIZES 50
+int sizes[NUM_SIZES];
+//const int sizes[NUM_SIZES] = { 509, 510, 511, 512, 513, 514, 515 };
 
 int main(int argc, char *argv[]){
 	srandom(time(NULL));
 	int i;
+	for(i = 0; i < NUM_SIZES; i++){
+		sizes[i] = 500 + i;
+	}
 	for(i = 0; i < NUM_SIZES; i++){
 		long long time_taken = 0L;
 		int j;
