289569659a43911942ebceb880850f049419b07c
m <m.rooney93@gmail.com>
2017-11-23 10:08:30 -0800

    Almost done

--- Assignment 3/cache.c
+++ Assignment 3/cache.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
 
 // TODO: read from file
@@ -35,8 +36,42 @@ int addresses[] = {
 	0x0070,
 	0x00d0,
 	0x0008,
+	0x3394,
+	0x0000,
+	0x0004,
+	0x000c,
+	0x2200,
+	0x00d0,
+	0x00e0,
+	0x1130,
+	0x0028,
+	0x113c,
+	0x2204,
+	0x0010,
+	0x0020,
+	0x0004,
+	0x0040,
+	0x2208,
+	0x0008,
+	0x00a0,
+	0x0004,
+	0x1104,
+	0x0028,
+	0x000c,
+	0x0084,
+	0x000c,
+	0x3390,
+	0x00b0,
+	0x1100,
+	0x0028,
+	0x0064,
+	0x0070,
+	0x00d0,
+	0x0008,
 	0x3394
 };
+
+int ADDRESS_ARRAY_SIZE = sizeof(addresses) / sizeof(int);
 
 struct line {
 	int initialised;
@@ -66,6 +101,7 @@ struct cache {
 	// If direct mapped then the first dimension is the same as num_blocks and the second dimension is 1.
 	// If fully associative then the first dimension is 1 and the second dimension is the same as num_blocks.
 	struct line **lines;
+	int sets_per_block;
 };
 
 struct cache c;
@@ -74,10 +110,17 @@ int NUM_HITS = 0;
 int NUM_MISSES = 0;
 
 // TODO:
-int set_cache_settings_from_args() {
-	c.total_size = 128;
-	c.line_size = 16;
-	c.associativity = 8;
+int set_cache_settings_from_args(int argc, char *argv[]) {
+	int i;
+	for(i = 1; i < 7; i = i + 2){
+		if(strcmp(argv[i], "-a") == 0){
+			c.associativity = atoi(argv[i+1]);
+		} else if(strcmp(argv[i], "-s") == 0){
+			c.total_size = atoi(argv[i+1]);
+		}  else if(strcmp(argv[i], "-l") == 0){
+			c.line_size = atoi(argv[i+1]);
+		}
+	}
 	return 0;
 }
 
@@ -106,12 +149,17 @@ void print_line_contents(int block, int set) {
 }
 
 
-int init_cache() {
-	set_cache_settings_from_args();
-	// TODO: fully associative represented with 0
+void init_cache(int argc, char *argv[]) {
+	set_cache_settings_from_args(argc, argv);
 	// TODO: increase this to 1 if it falls below that limit.
 	c.num_lines = c.total_size / c.line_size;
-	c.num_blocks = c.num_lines / c.associativity;
+	if (c.associativity != 0) {
+		c.num_blocks = c.num_lines / c.associativity;
+		c.sets_per_block = c.associativity;
+	} else {
+		c.num_blocks = 1;
+		c.sets_per_block = c.num_lines;
+	}
 	c.num_block_bits = find_number_power_of_two(c.num_blocks);
 	c.num_offset_bits = find_number_power_of_two(c.line_size);
 	c.lines = calloc(1, sizeof(struct line *) * c.num_blocks);
@@ -122,14 +170,14 @@ int init_cache() {
 	}
 	for (i = 0; i < c.num_blocks; i++) {
 		int n;
-		for (n = 0; n < c.associativity; n++) {
+		for (n = 0; n < c.sets_per_block; n++) {
 			c.lines[i][n].addresses = calloc(1, c.line_size); // in bytes, no need for sizeof(int)
 			c.lines[i][n].initialised = 0;
 			c.lines[i][n].cur_tag = 0;
 		}
 	}
-	printf("Num blocks: %d, num lines %d, num block bits: %d, num offset bits: %d\n", c.num_blocks, c.num_lines, c.num_block_bits, c.num_offset_bits);
-	return 0;
+	printf("Associativity: %d\nNum blocks: %d\nNum sets per block: %d \nNum lines %d\n", c.associativity, c.num_blocks, c.sets_per_block, c.num_lines);
+	printf("Num block bits: %d\nNum offset bits: %d\n", c.num_block_bits, c.num_offset_bits);
 }
 
 // We want to extract the block bits from the address
@@ -153,97 +201,92 @@ int extract_tag_bits(int address) {
 
 int block_contains_address(int tag, int block) {	
 	int i;
-	for (i = 0; i < c.associativity; i++) {
+	for (i = 0; i < c.sets_per_block; i++) {
 		if (c.lines[block][i].initialised == 0) {
 			continue;
 		}
 		if (c.lines[block][i].cur_tag == tag) {
-			printf("Line 0x%08x contains tag 0x%08x - hit!\n", block, tag);
+			//printf("Line 0x%08x contains tag 0x%08x - hit!\n", block, tag);
 			NUM_HITS++;
 			return 1;
 		}
 	}
-	printf("Block 0x%08x does NOT contain tag 0x%08x - miss!\n", block, tag);
+	//printf("Block 0x%08x does NOT contain tag 0x%08x - miss!\n", block, tag);
 	NUM_MISSES++;
 	return 0;
 }
 
-void load_addresses_into_block_direct_mapped(int tag, int block) {
-	if (c.lines[block][0].initialised == 1) {
-		printf("Evicting tag 0x%08x from block 0x%08x\n", tag, block);
-	}
-	int i;
-	for (i = 0; i < c.line_size; i++) {
-		c.lines[block][0].addresses[i] = tag + i;
-	}
-	c.lines[block][0].initialised = 1;
-	c.lines[block][0].cur_tag = tag;
-}
-
-void load_addresses_into_set_n_way(int tag, struct line *l, int count) {
+// No need to actually load the data here.
+// Just mark that the set contains the tag and set a counter to be used in LRU scheme.
+void load_addresses_into_line(int tag, struct line *l, int count) {
 	if (l->initialised == 1) {
-		printf("Evicting tag 0x%08x from line\n", tag);
-	}
-	int i;
-	for (i = 0; i < c.line_size; i++) {
-		l->addresses[i] = tag + i;
+		//printf("Evicting tag 0x%08x from line\n", l->cur_tag);
 	}
 	l->initialised = 1;
 	l->cur_tag = tag;
-	l->last_used = i + 1; // avoid zero case
+	l->last_used = count + 1; // avoid zero case
 }
 
 struct line * find_least_recently_used_line(struct line *block) {
+	//int lru = 0;
 	int i;
 	struct line *cur = &block[0];
-	if (cur->initialised == 0) {
-		return cur;
-	}
-	for (i = 1; i < c.associativity; i++) {
+	for (i = 0; i < c.sets_per_block; i++) {
 		if (block[i].initialised == 0) {
+			//printf("Set 0x%08x is not yet initialised - using it.\n", i);
 			return &block[i];
 		}
 		if (block[i].last_used < cur->last_used) {
+			//lru = i;
 			cur = &block[i];
 		}
 	}
+	//printf("Set 0x%08x is LRU - using it.\n", lru);
 	return cur;
 }
 
-// TODO: fully associative?
 void load_addresses_into_block(int tag, int block, int count) {
+	struct line *l;
 	if (c.associativity == 1) {
-		load_addresses_into_block_direct_mapped(tag, block);
+		l = c.lines[block];
 	} else {
 		struct line *line_block = c.lines[block];
-		struct line *l = find_least_recently_used_line(line_block);
-		load_addresses_into_set_n_way(tag, l, count);
+		l = find_least_recently_used_line(line_block);
 	}
-
+	load_addresses_into_line(tag, l, count);
 }
 
 // TODO: don't hardcode size
 void simulate_cache() {
 	int i;
-	for (i = 0; i < 32; i++) {
+	for (i = 0; i < ADDRESS_ARRAY_SIZE; i++) {
 		int address = addresses[i];
 		int tag_bits = extract_tag_bits(address);
-		int block_bits = extract_block_bits(address);
-		printf("Address is: 0x%08x\n", address);
+		int block_bits;
+		if (c.associativity == 0) {
+			block_bits = 0;
+		} else {
+			block_bits = extract_block_bits(address);
+		}
+		//printf("Address is: 0x%08x\n", address);
 		//printf("Block bits are: 0x%08x\n", block_bits);
 		//printf("Tag bits are: 0x%08x\n", tag_bits);
 		if (block_contains_address(tag_bits, block_bits) == 0) {
 			load_addresses_into_block(tag_bits, block_bits, i);
 		}
-		printf("\n");
+		//printf("\n");
 		//print_line_contents(i, n);
 	}
 }
 
-int main() {
-	init_cache();
+int main(int argc, char *argv[]) {
+	if(argc != 7){
+		printf("Expected usage: ./cache -s size -l linesize -a associativity\n");
+		return 1;
+	}
+	init_cache(argc, argv);
 	simulate_cache();
-	printf("Hits: %d, Misses: %d\n", NUM_HITS, NUM_MISSES);
+	printf("Hits: %d, Misses: %d, Ratio: %f\n", NUM_HITS, NUM_MISSES, ((float) NUM_HITS / (float) ADDRESS_ARRAY_SIZE));
 	return 0;
 }
 
